f27e4ca894c21a54e80f46a995de973091b6c47f
Lawrence La <La.Lawreneoebb.at>
2022-11-16 13:19:34 +0100

    Projektdateien hinzufügen.

---
+++ WuerfelProgramm15112022/MainWindow.xaml.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WuerfelProgramm15112022
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        static int count4 = 0;
+        static int count6 = 0;
+        static int count8 = 0;
+        static int count10 = 0;
+        static int count12 = 0;
+        static int count20 = 0;
+        static int count100 = 0;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, RoutedEventArgs e)
+        {
+            count4++;
+            TextBlock1.Text = $"{count4}x";
+
+        }
+
+        private void Button2_Click(object sender, RoutedEventArgs e)
+        {
+            count6++;
+            TextBlock2.Text = $"{count6}x";
+        }
+
+        private void Button3_Click(object sender, RoutedEventArgs e)
+        {
+            count8++;
+            TextBlock3.Text = $"{count8}x";
+        }
+
+        private void Button4_Click(object sender, RoutedEventArgs e)
+        {
+            count10++;
+            TextBlock4.Text = $"{count10}x";
+        }
+
+        private void Button5_Click(object sender, RoutedEventArgs e)
+        {
+            count12++;
+            TextBlock5.Text = $"{count12}x";
+        }
+
+        private void Button6_Click(object sender, RoutedEventArgs e)
+        {
+            count20++;
+            TextBlock6.Text = $"{count20}x";
+        }
+
+        private void Button7_Click(object sender, RoutedEventArgs e)
+        {
+            count100++;
+            TextBlock7.Text = $"{count100}x";
+        }
+
+        private void PlusButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MinusButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void EnterTaste_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ WuerfelProgramm15112022/Wuerfel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WuerfelProgramm15112022
+{
+    internal class Wuerfel
+    {
+        Random random = new Random();
+
+    }
+}
